d33e5df7bca305bce95cb67d23926f766023027f
Rei Arifi <rei.a@scopicsoftware.com>
2022-05-07 12:38:01 +0200

    changed kubeconfig location

---
+++ startvm.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+test -z $VM_NAME && echo 'Please specify VM_NAME' && exit 1
+
+if [[ -z $INIT ]]; then
+    test -z $K3S_URL && echo 'Please specify K3S_URL' && exit 1
+    test -z $K3S_TOKEN && echo 'Please specify K3S_TOKEN' && exit 1
+fi
+
+OVA=${OVA:-../coreos.ova}
+BUTANE=${BUTANE:-config.yml}
+IGNITION=${IGNITION:-config.ign}
+INSTALL_K3S_VERSION=${INSTALL_K3S_VERSION:-v1.23.6+k3s1}
+ADAPTER=$(ip route ls default | grep -Po '(?<= dev )(\S+)')
+
+
+envsubst < $BUTANE | butane --pretty --strict > $IGNITION
+vboxmanage import --vsys 0 --vmname "$VM_NAME" "$OVA"
+vboxmanage guestproperty set "$VM_NAME" /Ignition/Config "$(cat $IGNITION)"
+vboxmanage modifyvm "$VM_NAME" --nic1 bridged --nictype1 82545EM --bridgeadapter1 "$ADAPTER"
+vboxmanage startvm "$VM_NAME" --type headless
+
+rm $IGNITION
